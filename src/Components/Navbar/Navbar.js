dc436fea68047d73405ed7190a8ef20cf296c1c3
manju prasad <manjuprasad.4327@gmail.com>
2025-02-14 15:36:19 +0530

    Manju

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -5,13 +5,24 @@ import './Navbar.css';
 
 const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const toggleMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const closeDropdown = (e) => {
+    if (!e.target.closest('.dropdown')) {
+      setIsDropdownOpen(false);
+    }
+  };
+
   return (
-    <header className="navbar">
+    <header className="navbar" onClick={closeDropdown}>
       <div className="logo">Tare Zameen Par</div>
       <nav className={`navbar-menu ${isMobileMenuOpen ? 'mobile-menu' : ''}`}>
         <ul className="navbar-list">
@@ -19,17 +30,18 @@ const Navbar = () => {
           <li className="navbar-item"><Link to="/about">About Us</Link></li>
 
           {/* Dropdown for Services */}
-          <li className="navbar-item dropdown">
-            {/* <Link to="/services" className="dropdown-toggle">Services</Link> */}
-            <Link  className="dropdown-toggle">Services</Link>
-            <ul className="dropdown-list">
-              <li className="dropdown-item"><Link to="/services/spark-curiosity">Spark of Curiosity</Link></li>
-              <li className="dropdown-item"><Link to="/services/mobile-planetarium">Mobile Planetorium</Link></li>
-              <li className="dropdown-item"><Link to="/services/mobile-science">Mobile Science Lab</Link></li>
-              <li className="dropdown-item"><Link to="/services/webinar">Webinar</Link></li>
-              <li className="dropdown-item"><Link to="/services/jupiter-curiosity">Jupiter of Curiosity</Link></li>
-              <li className="dropdown-item"><Link to="/services/school-alliance">School of Alliance</Link></li>
-            </ul>
+          <li className={`navbar-item dropdown ${isDropdownOpen ? 'open' : ''}`} onClick={toggleDropdown}>
+            <Link className="dropdown-toggle">Services</Link>
+            {isDropdownOpen && (
+              <ul className="dropdown-list">
+                <li className="dropdown-item"><Link to="/services/spark-curiosity">Spark of Curiosity</Link></li>
+                <li className="dropdown-item"><Link to="/services/mobile-planetarium">Mobile Planetarium</Link></li>
+                <li className="dropdown-item"><Link to="/services/mobile-science">Mobile Science Lab</Link></li>
+                <li className="dropdown-item"><Link to="/services/webinar">Webinar</Link></li>
+                <li className="dropdown-item"><Link to="/services/jupiter-curiosity">Jupiter of Curiosity</Link></li>
+                <li className="dropdown-item"><Link to="/services/school-alliance">School of Alliance</Link></li>
+              </ul>
+            )}
           </li>
 
           <li className="navbar-item"><Link to="/csr">CSR</Link></li>
