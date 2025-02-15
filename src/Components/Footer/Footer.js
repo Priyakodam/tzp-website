7a443dd0b953b100a9aff39cfc5d821370bdd8c4
manju prasad <manjuprasad.4327@gmail.com>
2025-02-15 18:06:17 +0530

    Manju

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -12,55 +12,39 @@ const Footer = () => {
     <footer className="footer-root">
       <div className="footer-container">
         <div className="footer-content">
-          {/* <div className="mainfoogterdiv"> */}
-          <div className="footer-logo">
-            {/* <img
-               src={}
-              alt="Sadashri Logo"
-           
-            /> */}
-          </div>
-      
-          <div className='footermainsection'>
+          <div className="footermainsection">
+            
+            {/* Logo Section */}
+            <div className="footer-logo">
+              <h3>Tare Zameen Par</h3>
+            </div>
+
+            {/* Quick Menu */}
             <div className="footer-section">
-              <p style={{ color: 'black', fontSize: '22px' }} className="footer-heading ">Quick Menu</p>
-              <p>  <a href="/" className="footer-link">Home</a></p>
+              <h3 className="footer-heading">Quick Menu</h3>
+              <p><a href="/" className="footer-link">Home</a></p>
               <p><a href="/about" className="footer-link">About Us</a></p>
               <p><a href="/csr" className="footer-link">CSR</a></p>
               <p><a href="/gallery" className="footer-link">Gallery</a></p>
               <p><a href="/contact" className="footer-link">Contact Us</a></p>
             </div>
+
+            {/* Contact Details */}
             <div className="footer-section">
-              <p style={{ color: 'black', fontSize: '22px' }} className="footer-heading foot-head">Contact Details</p>
+              <h3 className="footer-heading">Contact Details</h3>
               <p className="footer-info">#52, 2nd Floor, 9th Main Road, Kaveri Nagar,</p>
               <p className="footer-info">Banashankari Stage II, Banashankari,</p>
-              {/* <p className="footer-info">Phone No: 9964644424</p> */}
-              {/* <p className="footer-info">Locality: Yelahanka New Town</p> */}
               <p className="footer-info">City: Bengaluru</p>
               <p className="footer-info">State: Karnataka, 560070</p>
             </div>
           </div>
         </div>
-
         <hr className="footer-divider" />
-        {/* <p className="footer-copyright">
-          Copyright © 2024 SadāShri Ventures Private Limited. All rights reserved.
-        </p> */}
-
         <section className="last-footer">
           <p className="last-footer-copyright">
             Copyright © 2024 SadāShri Ventures Private Limited. All rights reserved.
           </p>
-          {/* <p className="last-footer-design">
-          Designed by <a href="https://www.iiiqbets.com/" target="_blank" rel="noopener noreferrer"> <img 
-        src={footerlogo} 
-        alt="iiiQBets Logo" 
-        className="footer1-logo" 
-      /></a>.
-          </p> */}
-          {/* <p className="last-footer-design">
-          Designed by <a href="https://www.iiiqbets.com/" target="_blank" rel="noopener noreferrer">iiiQBets</a>.
-          </p> */}
+         
           <div className="footer">
             <span className="float-left pr-2">
               © {new Date().getFullYear()} All rights reserved 
